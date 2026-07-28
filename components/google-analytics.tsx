3e5d152efa79dba55738d606f413debc11d0e892
DevCoreDXI76 <devcoredxi00@coredxi.com>
2026-07-28 14:55:13 +0900

    fix: GA4 page_location에서 쿼리 제거 + privacy 문구 정확도 수정 + 배포 체크리스트 보강

--- components/google-analytics.tsx
+++ components/google-analytics.tsx
@@ -18,7 +18,9 @@ export function GoogleAnalytics() {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${measurementId}');
+          gtag('config', '${measurementId}', {
+            page_location: location.origin + location.pathname
+          });
         `}
       </Script>
     </>
